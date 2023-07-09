89c45f83bf5d805635d9109feaed0eeeca0faff0
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2023-07-09 16:32:32 +0300

    Add `play` button

--- timer/src/main/java/io/vbytsyuk/timer/TimerScreen.kt
+++ timer/src/main/java/io/vbytsyuk/timer/TimerScreen.kt
@@ -1,7 +1,11 @@
 package io.vbytsyuk.timer
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,10 +18,19 @@ fun TimerScreen(time: Time) {
     Box(
         modifier = Modifier.fillMaxSize(),
     ) {
-        Timer(
-            time = time,
-            modifier = Modifier.align(Alignment.Center)
-        )
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+        ) {
+            Timer(
+                time = time,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+            )
+            Button(
+                onClick = {},
+            ) {
+                Text(text = "Play")
+            }
+        }
     }
 }
 
@@ -37,6 +50,7 @@ private fun Timer_tablet() = TimerScreen(PREVIEW_TIME)
 
 @Preview(
     device = Devices.WEAR_OS_LARGE_ROUND,
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_WATCH,
 )
 @Composable
 private fun Timer_wear() = TimerScreen(PREVIEW_TIME)
